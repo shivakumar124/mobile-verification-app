7bb92279f12ff6490bca2d3313a8a920d2de7e20
Shiva2466 <shiva@reach52.com>
2024-10-23 22:59:19 +0530

    code changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user.routes');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
+const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 
@@ -15,6 +16,15 @@ app.use('/api/users', userRoutes);
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
+});
+
+app.get('/',(req, res)=>{
+    res.send("Welcome to Mobile Verification APP!!!")
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
